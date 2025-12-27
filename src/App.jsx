12a9a51bcbd3b28150b2ed8761999ab59827e979
pradeep-jais <pradeepjais13@gmail.com>
2025-12-27 20:26:26 +0530

    Initial commit - recovered from corrupted state - git and node_modules corrupted due to onedrive sync, lost projects and components

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './App.css';
+import FetchData from './projects/00_data_fetch';
+import BlogCard from './projects/01_blog_card';
+import { projectMenu } from './projectData';
+
+function App() {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [activeProject, setActiveProject] = useState('00_data_fetch');
+
+  const selectProject = (path) => {
+    console.log(path);
+    setActiveProject(path);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="max-width">
+          <button
+            className="sidebar-toggle-btn"
+            onClick={() => setToggleSidebar(!toggleSidebar)}
+          >
+            &#9776;
+          </button>
+          <span>☯ Project hub</span>
+        </div>
+      </nav>
+      <div className="layout">
+        <aside className={`sidebar ${toggleSidebar ? '' : 'hide'}`}>
+          <h3>projects</h3>
+          <ul>
+            {projectMenu.map((project) => {
+              const { id, name, path } = project;
+              return (
+                <li key={id} onClick={() => selectProject(path)}>
+                  {name}
+                </li>
+              );
+            })}
+          </ul>
+        </aside>
+        <main>
+          <h1>Explore my practice project app</h1>
+          <hr />
+          {/* <FetchData /> */}
+          {activeProject === '00_data_fetch' && <FetchData />}
+          {activeProject === '01_blog_card' && <BlogCard />}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/projectData.js
@@ -0,0 +1,17 @@
+export const projectMenu = [
+  {
+    id: 1,
+    name: 'Data Fetching',
+    path: '00_data_fetch',
+  },
+  {
+    id: 2,
+    name: 'Blog post',
+    path: '01_blog_card',
+  },
+  {
+    id: 3,
+    name: 'card UI',
+    path: '02_card_ui',
+  },
+];
